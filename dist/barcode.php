e0b2cb5c3020fe029f8d683ab36974b9438f5555
Krisada Jamchat <jam.krisada@gmail.com>
2016-10-22 07:38:58 +0700

    init

---
+++ dist/barcode.php
@@ -0,0 +1,14 @@
+<?php
+
+$barcode = array(
+  array(
+    "barcode" => "079656603682",
+    "name" => "Banana Boat Sport Coolzone Instantly Cools amp Refreshes Sunscreen Lotion Continuous Spray SPF UVB 50 PA 6oz 170g",
+    "description" => "Banana Boat Sport Coolzone Instantly Cools & Refreshes Sunscreen Lotion Continuous Spray SPF / UVB 50+ / PA+++ has the following features:Its fresh scent stimulates the senses while the aloe-enriched formula leaves you feeling cool and refreshed. AvoTriplex™ broad spectrum UVA/UVB formula provides resilient water-resistant protection for a sport activity. Invisible and rub-free.",
+    "images" => array(
+      "http://www.cosme-de.com/product_photo/b00000601/15511_1.jpg"
+    ),
+    "category" => "Sun Care",
+    "brand" => "Banana Boat"
+  )
+);
